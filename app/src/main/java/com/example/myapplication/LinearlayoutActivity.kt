c82b300b0eab45f18e9c4d1562a6549b9e5fb505
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2023-10-05 01:50:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/LinearlayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearlayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linearlayout)
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TableFrameActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableFrameActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_frame)
+    }
+}
